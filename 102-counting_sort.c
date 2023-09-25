8f6c2a2d663e9f2e261ab274a4970aec843b1d78
Ng-RobGithub <ngrob4real@gmail.com>
2023-09-25 04:06:04 -0700

    update on counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -2,63 +2,55 @@
 
 /**
  * counting_sort - Sorts an array of integers in ascending order using
- * the Counting Sort algorithm.
- * @array: The array of integers to be sorted.
+ * the Counting sort algorithm.
+ * @array: The array to be sorted.
  * @size: The size of the array.
  */
 void counting_sort(int *array, size_t size)
 {
-	int max, min;
-	size_t i, range;
+	int max = 0;
+	size_t i;
 	int *counting_array, *output_array;
 
 	if (array == NULL || size < 2)
 		return;
 
-	max = min = array[0];
-
-	/* Find the minimum and maximum values in the array */
+	/* Find the maximum element in the array */
 	for (i = 0; i < size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
-		if (array[i] < min)
-			min = array[i];
 	}
 
-	/* Calculate the range of values */
-	range = max - min + 1;
-
-	/* Allocate memory for the counting array */
-	counting_array = malloc(range * sizeof(int));
+	/* Create a counting array of size (max + 1) */
+	counting_array = malloc(sizeof(int) * (max + 1));
 	if (counting_array == NULL)
 		return;
 
 	/* Initialize the counting array with zeros */
-	for (i = 0; i < range; i++)
+	for (i = 0; i <= (size_t)max; i++)
 		counting_array[i] = 0;
 
-	/* Count the occurrences of each value in the array */
+	/* Count the occurrences of each element in the original array */
 	for (i = 0; i < size; i++)
-		counting_array[array[i] - min]++;
+		counting_array[array[i]]++;
 
-	/* Update the counting array with cumulative counts */
-	for (i = 1; i < range; i++)
+	/* Update the counting array to store the cumulative count */
+	for (i = 1; i <= (size_t)max; i++)
 		counting_array[i] += counting_array[i - 1];
 
-	/* Allocate memory for the output array */
-	output_array = malloc(size * sizeof(int));
+	/* Create the output array and place elements in their sorted order */
+	output_array = malloc(sizeof(int) * size);
 	if (output_array == NULL)
 	{
 		free(counting_array);
 		return;
 	}
 
-	/* Build the output array using the counting array */
-	for (i = size - 1; i < (size_t)-1; i--)
+	for (i = 0; i < size; i++)
 	{
-		output_array[counting_array[array[i] - min] - 1] = array[i];
-		counting_array[array[i] - min]--;
+		output_array[counting_array[array[i]] - 1] = array[i];
+		counting_array[array[i]]--;
 	}
 
 	/* Copy the sorted elements back to the original array */
@@ -66,7 +58,10 @@ void counting_sort(int *array, size_t size)
 		array[i] = output_array[i];
 
 	/* Print the counting array (optional) */
-	print_array(counting_array, range);
+	printf("Counting array:");
+	for (i = 0; i <= (size_t)max; i++)
+		printf(" %d", counting_array[i]);
+	printf("\n");
 
 	/* Free dynamically allocated memory */
 	free(counting_array);
