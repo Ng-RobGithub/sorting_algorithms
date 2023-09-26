f2115aaaf71586bf11f33c9888752ca04da27d6d
Ng-RobGithub <ngrob4real@gmail.com>
2023-09-26 13:23:47 -0700

    sorts an array of integers in ascending order using the Counting sort..update2

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,51 +1,69 @@
 #include "sort.h"
 
 /**
- * print_counting_array - Prints the counting array.
- * @counting_array: The counting array.
- * @max_value: The maximum value in the original array.
- */
-void print_counting_array(int *counting_array, int max_value)
-{
-	/* Implementation of printing the counting array */
-}
-
-/**
- * counting_sort - Sorts an array of integers in ascending order
+ * counting_sort - Sort an array of integers in ascending order
  * using the Counting sort algorithm.
- * @array: The array to be sorted.
+ * @array: An array of integers.
  * @size: The size of the array.
+ *
+ * Description: This function sorts an array of non-negative integers
+ * using the Counting sort algorithm. It assumes that the input array
+ * contains only non-negative numbers.
  */
 void counting_sort(int *array, size_t size)
 {
-	int max_value;
-	int *counting_array;
-	int *sorted_array;
+	int *counting_array, *sorted_array;
+	int max_value = 0, i;
 
 	if (array == NULL || size < 2)
 		return;
 
-	if (array == NULL || size < 2)
-		return;
+	/* Find the maximum value in the input array */
+	for (i = 0; i < (int)size; i++)
+	{
+		if (array[i] > max_value)
+			max_value = array[i];
+	}
 
-	max_value = find_max_value(array, size);
-	*counting_array = create_counting_array(array, size, max_value);
-
+	/* Create a counting array of size (max_value + 1) and initialize to 0 */
+	counting_array = malloc(sizeof(int) * (max_value + 1));
 	if (counting_array == NULL)
 		return;
 
-	update_cumulative_counts(counting_array, max_value);
-	*sorted_array = create_sorted_array(array, size, counting_array);
+	for (i = 0; i <= max_value; i++)
+		counting_array[i] = 0;
 
+	/* Count the occurrences of each element in the input array */
+	for (i = 0; i < (int)size; i++)
+		counting_array[array[i]]++;
+
+	/* Update the counting array to store the cumulative counts */
+	for (i = 1; i <= max_value; i++)
+		counting_array[i] += counting_array[i - 1];
+
+	/* Create a sorted array using the counting array */
+	sorted_array = malloc(sizeof(int) * size);
 	if (sorted_array == NULL)
 	{
 		free(counting_array);
 		return;
 	}
 
-	copy_sorted_array(array, sorted_array, size);
-	print_counting_array(counting_array, max_value);
+	for (i = size - 1; i >= 0; i--)
+	{
+		sorted_array[counting_array[array[i]] - 1] = array[i];
+		counting_array[array[i]]--;
+	}
 
+	/* Copy the sorted array back to the original array */
+	for (i = 0; i < (int)size; i++)
+		array[i] = sorted_array[i];
+
+	/* Print the counting array as per the requirement */
+	printf("Array after counting:\n");
+	print_array(counting_array, max_value + 1);
+
+	/* Free dynamically allocated memory */
 	free(counting_array);
 	free(sorted_array);
 }
