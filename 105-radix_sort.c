e259de9d545dbad718d182653481f3f980bfc3bc
Ng-RobGithub <ngrob4real@gmail.com>
2023-09-25 18:49:35 -0700

    sorts an array of integers in ascending order using the Radix sort update2

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,26 +1,25 @@
 #include "sort.h"
-
-int get_max(int *array, int size);
-void radix_counting_sort(int *array, size_t size, int sig, int *buff);
-void radix_sort(int *array, size_t size);
+#include <stdlib.h>
 
 /**
  * get_max - Get the maximum value in an array of integers.
  * @array: An array of integers.
  * @size: The size of the array.
- *
  * Return: The maximum integer in the array.
  */
 int get_max(int *array, int size)
 {
-	int max, i;
+	int i, max;
 
-	for (max = array[0], i = 1; i < size; i++)
+	max = array[0];
+
+	for (i = 1; i < size; i++)
 	{
 		if (array[i] > max)
+		{
 			max = array[i];
+		}
 	}
-
 	return (max);
 }
 
@@ -34,13 +33,14 @@ int get_max(int *array, int size)
  */
 void radix_counting_sort(int *array, size_t size, int sig, int *buff)
 {
-	int count[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	size_t i;
+
+	int count[10] = {0};
 
 	for (i = 0; i < size; i++)
 		count[(array[i] / sig) % 10] += 1;
 
-	for (i = 0; i < 10; i++)
+	for (i = 1; i < 10; i++)
 		count[i] += count[i - 1];
 
 	for (i = size - 1; (int)i >= 0; i--)
@@ -64,16 +64,17 @@ void radix_counting_sort(int *array, size_t size, int sig, int *buff)
  */
 void radix_sort(int *array, size_t size)
 {
-	int max, sig, *buff;
+	int max, *buff, sig;
 
 	if (array == NULL || size < 2)
 		return;
 
+	max = get_max(array, size);
 	buff = malloc(sizeof(int) * size);
+
 	if (buff == NULL)
 		return;
 
-	max = get_max(array, size);
 	for (sig = 1; max / sig > 0; sig *= 10)
 	{
 		radix_counting_sort(array, size, sig, buff);
